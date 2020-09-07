651d1957d9672fd19609edcb77e41bd56d446098
Philip Lind Jensen <jogjensen@hotmail.com>
2020-09-07 10:18:39 +0200

    Add project files.

---
+++ Bike/Bike.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Bike
+{
+    public class Bike
+    {
+
+        static void Main(string[] args)
+        {
+        }
+
+        #region Instance fields
+
+        private int _id;
+        private string _color;
+        private int _price;
+        private int _gear;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                if (value.Length < 1)
+                    throw new ArgumentException();
+                else _color = value;
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (1 <= value) _price = value;
+                else throw new ArgumentOutOfRangeException("Price", value, "Illegal price");
+            }
+        }
+
+        public int Gear
+        {
+            get { return _gear; }
+            set
+            {
+                if (3 <= value && value <= 23) _gear = value;
+                else throw new ArgumentOutOfRangeException("Gear", value, "Gear out of range");
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Bike(int id, string color, int price, int gear)
+        {
+            Id = id;
+            Color = color;
+            Price = price;
+            Gear = gear;
+        }
+
+
+        #endregion
+
+    }
+}
+
---
+++ BikeTests/BikeTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bike;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bike.Tests
+{
+    [TestClass()]
+    public class BikeTests
+    {
+
+        private Bike bike = new Bike(1,"Yellow",1000,17);
+
+        [TestMethod()]
+        public void BikeConstructorTest()
+        {
+            Bike cykel = new Bike(1,"Yellow",1000,17);
+
+            Assert.AreEqual(1,bike.Id);
+            Assert.AreEqual("Yellow",bike.Color);
+            Assert.AreEqual(1000,bike.Price);
+            Assert.AreEqual(17,bike.Gear);
+        }
+
+        [TestMethod]
+        public void IdTest()
+        {
+            bike.Id = 1;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestColor1Char()
+        {
+            bike.Color = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPriceOverMustBeOver0()
+        {
+            bike.Price = -1;
+        }
+
+        [TestMethod]
+        public void TestPriceCanBe1()
+        {
+            bike.Price = 1;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGearOutOfRange()
+        {
+            bike.Gear = 2;
+        }
+
+        [TestMethod]
+        public void TestGearInRange()
+        {
+            bike.Gear = 3;
+        }
+    }
+}
